328763ff3c84a5dd20b3a5002e0a5fa565817566
valerie Tshuma <hlulanivalerietshuma@gmail.com>
2024-11-25 14:47:58 +0200

    Added the user authentication

---
+++ src/main/java/com/Scoders/BankingApp/controller/auth/Authenticate.java
@@ -0,0 +1,21 @@
+package com.Scoders.BankingApp.controller.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Authenticate {
+
+@GetMapping ("/")
+    public String index(){
+    return "index"; // Render index.html
+}
+@GetMapping("/register")
+    public String register(){
+    return "register";
+}
+@GetMapping("/login")
+    public String login(){
+    return "login";
+}
+}
